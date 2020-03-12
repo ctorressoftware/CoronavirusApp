c44a73fd0f14d8e4b8be5bf83ad246b16b0e2cfc
Mr. Carlos <carlostorresapec@hotmail.com>
2020-03-12 17:02:11 -0400

    Agregar pagination. Solamente 4 noticias por pantalla.

--- CoronavirusApp/Controllers/NoticiaController.cs
+++ CoronavirusApp/Controllers/NoticiaController.cs
@@ -1,15 +1,17 @@
 ﻿using CoronavirusApp.Models;
 using System.Web.Mvc;
+using PagedList.Mvc;
+using PagedList;
 
 namespace CoronavirusApp.Controllers
 {
     public class NoticiaController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(int? page)
         {
             NoticiaParser parser = new NoticiaParser();
 
-            return View(parser.ObtenerNoticias());
+            return View(parser.ObtenerNoticias().ToPagedList(page ?? 1, 4));
         }
     }
 }
