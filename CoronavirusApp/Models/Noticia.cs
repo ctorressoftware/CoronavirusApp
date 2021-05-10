a9def9aa0365d16e4e3e4d32f52640356b5151da
DESKTOP-R5UNV7E\Carlos <ctorressoftware@hotmail.com>
2021-05-09 21:18:15 -0400

    Se agregó enlace hacia cada noticia

--- CoronavirusApp/Models/Noticia.cs
+++ CoronavirusApp/Models/Noticia.cs
@@ -7,5 +7,6 @@ namespace CoronavirusApp.Models
         public string Titulo { get; set; }
         public DateTime FechaPublicacion { get; set; }
         public string Descripcion { get; set; }
+        public string Link { get; set; }
     }
 }
